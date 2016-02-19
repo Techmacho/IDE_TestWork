648ea8fcb46f0f7d407e90efa5401a292a5b76b8
Techmacho <adamgomezjr1@gamil.com>
2016-02-19 14:48:46 -0700

    Added foo variable to sh function

--- foofoo.py
+++ foofoo.py
@@ -2,16 +2,16 @@ import cherrypy
 
 class HelloWorld(object):
     def index(self):
-        return "<a href='/sh'>Test me</a>"
+        return "<a href='/sh?foo=test'>Test me</a>"
     index.exposed = True
 
-    def sh(self):
+    def sh(self,foo=None):
         from sense_hat import SenseHat
 
         s=SenseHat()
-        s.show_message("testing")
+        s.show_message(foo)
         return "Was that cool?"
     sh.exposed = True
 
-
+cherrypy.config.update({'server.socket_host': '0.0.0.0'})
 cherrypy.quickstart(HelloWorld())
